22cc10a7dec67c6b9fe3654000631f508964b44f
c.ishii <chiichiitei_0711@yahoo.co.jp>
2022-04-13 04:48:57 +0900

    会社詳細ページURLを取得

--- duda.py
+++ duda.py
@@ -3,20 +3,26 @@ import requests
 from bs4 import BeautifulSoup
 from time import sleep
 
+# 企業詳細ページURLリストを取得
 d_list = []
-i = 1
+i = 0
 while True:
+  i += 1
   url='https://doda.jp/DodaFront/View/JobSearchList.action?pic=1&ds=0&oc=0313M%2C0317M%2C0321M&so=50&ci=401307&pf=0&tp=1&page=' + str(i)
   print(url)
+  res = requests.get(url, timeout=3, allow_redirects=False)
+  print(res.status_code)
+  #print(res.history)
+  #res.raise_for_status()
+  if res.status_code != 200:
+    break
 
-  r = requests.get(url)
-
-  soup = BeautifulSoup(r.content, 'html.parser')
+  soup = BeautifulSoup(res.content, 'html.parser')
   post = soup.find('section', id='shStart')
 
   for h2 in post.find_all('h2'):
     detail_url = h2.find('a').get('href')
-    sleep(0.5)
+    sleep(0.01)
     # post_r = requests.get(post_url)
     # post_soup = BeautifulSoup(post_r.content, 'html.parser')
     # post_h3 = [h3.text for h3 in post_soup.find_all('h3')]
@@ -25,14 +31,13 @@ while True:
     }
     d_list.append(d)
 
-  print('====='+str(i)+'======')
-  i += 1
-
-  if i>10:
-    break
-
 print(len(d_list))
 
+# 詳細ページを読んで、会社名、会社URLを取得
+
+
+
+# CSV出力
 df=pd.DataFrame(d_list)
 
 print(df)
