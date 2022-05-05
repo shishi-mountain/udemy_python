a5e4f247100acc23a682ff5abad52ac7f62a9839
c.ishii <chiichiitei_0711@yahoo.co.jp>
2022-05-06 05:04:20 +0900

    #7 exp

--- rikunabi_exp.py
+++ rikunabi_exp.py
@@ -6,11 +6,11 @@ import re
 
 # 企業詳細ページURLリストを取得
 base_url = 'https://next.rikunabi.com'
-d_list = []
+company_list = []
 i = 0
 while True:
   # test
-  if i>3:
+  if i>1:
    break
 
   url='https://next.rikunabi.com/rnc/docs/cp_s00700.jsp?jb_type_long_cd=0500000000&wrk_plc_long_cd=0840000000&wrk_plc_long_cd=0840300000&curnum=' + str(1+50*i)
@@ -24,58 +24,30 @@ while True:
 
   soup = BeautifulSoup(res.content, 'html.parser')
   post = soup.select('a:-soup-contains(企業ページ)')
-  for pp in post:
-    detail_url = base_url + pp.get('href')
-    sleep(1)
-    d_list.append(detail_url)
-    # print(detail_url)
+  for page_url in post:
+    page_url = base_url + page_url.get('href')
 
-# 詳細ページを読んで、求人詳細ページにとぶ
-company_list = []
-for d_url in d_list:
-  r = requests.get(d_url, timeout=3)
-  soup = BeautifulSoup(r.content, 'html.parser')
+    sleep(0.1)
 
-  # 求人情報URL取得
-  if (soup.select_one('.rn3-companyOfferTabMenu__navItemLink') == None):
-    # 求人詳細ページがない場合は読み飛ばす
-    continue
+    page_r = requests.get(page_url, timeout=3)
+    page_r.raise_for_status()
 
-  info_url = base_url + soup.select_one('.rn3-companyOfferTabMenu__navItemLink').get('href')
-  info_r = requests.get(info_url, timeout=3)
-  soup = BeautifulSoup(info_r.content, 'html.parser')
-  if (soup.select_one('.rn3-companyOfferCompany__link') == None):
-    # 社名なし
-    continue
+    page_soup = BeautifulSoup(page_r.content, 'html.parser')
 
-  # 会社名
-  company_name = soup.select_one('.rn3-companyOfferCompany__link').text
+    company_name = page_soup.select_one('.rnn-breadcrumb > li:last-of-type')
+    print('='*50)
+    print(company_name.text)
+    # aがrnn-col-11 の直下ではないから、>はつけない
+    url_in_tag = page_soup.select_one('.rnn-col-11:last-of-type a')
+    # 三項演算子
+    company_url = url_in_tag.get('href') if url_in_tag else None
+    print(company_url)
 
-  if (soup.select_one('.js-companyOfferEntry__link') == None):
-    # 会社URLなし
-    continue
-
-  # 会社URLに飛ぶ前のアクセスURL
-  tmp_url = soup.select_one('.js-companyOfferEntry__link').get('href')
-
-  tmp_r = requests.get(base_url + tmp_url)
-  soup = BeautifulSoup(tmp_r.content, 'html.parser')
-  # company_url = soup.find('a', text=re.compile("こちら"))
-  company_url = soup.select_one('a:-soup-contains("こちら")')
-  if company_url is None:
-    # URLなし
-    company_url = ''
-  else:
-    company_url = company_url.get('href')
-
-  c_info = {
-    'company': company_name.replace('\r\n','').replace('\n','').replace(' ', ''),
-    'url': company_url,
-  }
-  company_list.append(c_info)
-  #print(c_info)
-
-
+    c_info = {
+      'company': company_name.text,
+      'url': company_url,
+    }
+    company_list.append(c_info)
 
 # CSV出力
 df=pd.DataFrame(company_list)
